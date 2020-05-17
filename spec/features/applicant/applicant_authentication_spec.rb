26946582335030e29630d97260b4a7b332094955
marlon <marlon_veiga@hotmail.com>
2020-05-17 01:30:01 -0300

    Adicionados testes conforme factory bot e alterada tradução yml.

--- spec/features/applicant/applicant_authentication_spec.rb
+++ spec/features/applicant/applicant_authentication_spec.rb
@@ -3,7 +3,7 @@ require  'rails_helper'
 feature 'Applicant authentication' do
   context 'log in' do
     scenario 'successfully' do
-      applicant = Applicant.create!(email: 'teste@teste.com', password: '123456789')
+      applicant = create(:applicant)
 
       visit root_path
       click_on 'Candidato'
@@ -21,7 +21,7 @@ feature 'Applicant authentication' do
     end
 
     scenario 'and must fill all fields' do
-      applicant = Applicant.create!(email: 'teste@teste.com', password: '123456789')
+      applicant = create(:applicant)
 
       visit root_path
       click_on 'Candidato'
@@ -38,7 +38,7 @@ feature 'Applicant authentication' do
 
   context 'log out' do
     scenario 'successfully' do
-      applicant = Applicant.create!(email: 'teste@teste.com', password: '123456789')
+      applicant = create(:applicant)
 
       visit root_path
       click_on 'Candidato'
@@ -73,6 +73,41 @@ feature 'Applicant authentication' do
       expect(page).to have_content('Login efetuado com sucesso.')
       expect(page).to have_link('Sair')
       expect(page).not_to have_link('Entrar')
+    end
+
+    scenario 'and email is already used' do
+      applicant = create(:applicant)
+      visit root_path
+      click_on 'Candidato'
+      click_on 'Cadastre-se'
+
+      fill_in 'Email', with: applicant.email
+      fill_in 'Senha', with: applicant.password
+      fill_in 'Confirmação de senha', with: applicant.password
+      within 'form' do
+        click_on 'Cadastre-se'
+      end
+
+      expect(page).to have_content('Email já está em uso')
+      expect(page).to have_link('Entrar')
+      expect(page).not_to have_link('Sair')
+    end
+
+    scenario 'and password didn\'t match the requirements' do
+      visit root_path
+      click_on 'Candidato'
+      click_on 'Cadastre-se'
+
+      fill_in 'Email', with: 'teste@teste.com'
+      fill_in 'Senha', with: '123'
+      fill_in 'Confirmação de senha', with: '123'
+      within 'form' do
+        click_on 'Cadastre-se'
+      end
+
+      expect(page).to have_content('Senha é muito curta (mínimo: 6 caracteres)')
+      expect(page).to have_link('Entrar')
+      expect(page).not_to have_link('Sair')
     end
   end
 end
