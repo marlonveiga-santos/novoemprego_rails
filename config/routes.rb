e8e1b571c914e5fc2931ba060fe5cd67ae84595d
marlon <marlon_veiga@hotmail.com>
2020-05-17 02:39:06 -0300

    Ajuste para receber model Profile.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :applicants
+  namespace :applicants do
+    root :to => "home#index" #profile page
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
