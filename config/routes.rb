89d4d4f84ecb64b6ada6e395249ff5f5a3276a0c
marlon <marlon_veiga@hotmail.com>
2020-05-22 19:32:45 -0300

    Reposicionada navbar e correções de rota.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :applicants
+  devise_for :applicants, controllers: {registrations: 'applicants/registrations'}
   resources :profiles, only: [:show, :edit, :update]
   
   get '/applicants' => "profiles#show", :as => :applicant_root
